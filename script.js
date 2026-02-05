bfcd91699e2fc570a2ca5e6d32153846d9507caf
Shittuadedamola <shittuadedamolabarry@gmail.com>
2026-02-05 01:40:01 +0100

    done till further development in the nearest future

---
+++ script.js
@@ -0,0 +1,36 @@
+const noButtonWordings = ["No", "Are you sure?", "Are you sure you're sure?", "You better say yes", 
+    "Babe pleeeeeaaasseee🥺", "Can't you say yes babe?", "You really mean it?"
+]
+
+var ini = 0
+var siz = 30
+
+const btns = document.querySelectorAll(".btn");
+const container = document.querySelector(".container");
+const yesContainer = document.querySelector(".yes-container");
+
+window.addEventListener('DOMContentLoaded', function(){
+    btns[1].innerHTML = noButtonWordings[ini]
+})
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        const answer = e.target.classList
+        
+        if (answer.contains("yes")){
+            container.style.display = "none"
+            yesContainer.style.display = "flex"
+        }
+
+        else if(answer.contains("no")){
+            ini++
+            if (ini >= noButtonWordings.length) {
+                ini = noButtonWordings.length - 1;
+            }
+            btns[1].innerHTML = noButtonWordings[ini]
+            siz+=8
+            btns[0].style.width = siz+"%";
+            btns[0].style.height = siz+"%"
+        }
+    })
+})
